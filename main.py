d3031325e4999e1d61f024a4bc17b540da6067e5
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-02-23 01:00:22 +0100

    Code refactoring

--- main.py
+++ main.py
@@ -1,222 +1,204 @@
 from tqdm import tqdm
 
 
-class Solver:
-    def __init__(self, file_name):
-        self.pizza_count, self.t_2, self.t_3, self.t_4 = [0, 0, 0, 0]
-        self.teams_count = 0
+class IO:
+    def __init__(self, input_file_name):
+        self.pizza_count = 0
+        self.team_count_dict = {"2": 0, "3": 0, "4": 0}
+
         self.pizza_list = []
         self.sorted_pizza_list = []
-        self.merged_pizza_list = []
-        self.team_dict = {}
         self.all_ingredients = set()
 
-        self.current_team_id = None
-        self.current_count = None
-        self.filled_teams_count = 0
-        self.is_start = True
-        self.can_work = True
+        self.team_dict = {}
+        self.full_teams_count = 0
 
         self.input_path_prefix = './input/'
         self.output_path_prefix = './output/'
-        self.file_name = file_name
-        self.read_file()
+        self.input_file_name = input_file_name
+        self.read_input()
 
-    def read_file(self):
-        with open(self.input_path_prefix + self.file_name) as file:
+    def read_input(self):
+        with open(self.input_path_prefix + self.input_file_name) as file:
             header_line = file.readline()
-            self.pizza_count, self.t_2, self.t_3, self.t_4 = [int(i) for i in header_line.strip().split()]
-            self.teams_count = self.t_2 + self.t_3 + self.t_4
+            self.pizza_count, two_teams, three_teams, four_teams = [int(i) for i in header_line.strip().split()]
+            self.initialize_pizza_list(file)
 
-            pizza_id = 0
-            for line in file.readlines():
-                ingredients = line.strip().split(' ')
-                ingredients_count = int(ingredients.pop(0))
-                self.all_ingredients = self.all_ingredients.union(set(ingredients))
+            self.team_count_dict = {"2": two_teams, "3": three_teams, "4": four_teams}
+            self.initialize_team_dicts(self.team_count_dict)
 
-                pizza = {
-                    "id": pizza_id,
-                    "ingredients": ingredients,
-                    "ingredients_count": ingredients_count,
-                    "assigned_team": None
-                }
+    def initialize_pizza_list(self, input_file):
+        for pizza_id, line in enumerate(input_file.readlines()):
+            ingredients = line.strip().split(' ')
 
-                self.pizza_list.append(pizza)
-                pizza_id += 1
+            self.pizza_list.append({
+                "id": pizza_id,
+                "ingredients": ingredients
+            })
+            self.all_ingredients = self.all_ingredients.union(set(ingredients))
 
-            self.team_dict = {
-                "2": {
-                    "first_free_id": 0,
-                    "list": self.get_empty_team_list(2, self.t_2)
-                },
-                "3": {
-                    "first_free_id": 0,
-                    "list": self.get_empty_team_list(3, self.t_3)
-                },
-                "4": {
-                    "first_free_id": 0,
-                    "list": self.get_empty_team_list(4, self.t_4)
-                }
+    def initialize_team_dicts(self, team_count_dict):
+        team_objects = map(
+            lambda pizza_count: self.get_empty_team_list(pizza_count),
+            team_count_dict.keys()
+        )
+        self.team_dict = dict(zip(team_count_dict.keys(), team_objects))
+
+    def get_empty_team_list(self, pizza_count):
+        return [
+            {
+                "id": team_id,
+                "pizzas": [],
+                "max_pizzas": int(pizza_count)
             }
+            for team_id in range(self.team_count_dict[pizza_count])
+        ]
 
-    @staticmethod
-    def get_empty_team_list(max_pizzas, count):
-        return [{"id": i, "pizzas": [], "max_pizzas": max_pizzas, "points": 0} for i in range(count)]
+    def write_output(self):
+        with open(self.output_path_prefix + self.input_file_name, 'w+') as file:
+            file.write(str(self.full_teams_count))
 
+            for count in self.team_dict:
+                for team in self.team_dict[count]:
+                    if team["max_pizzas"] == len(team["pizzas"]):
+                        line = "\n" + str(team["max_pizzas"]) + " "
+                        line += " ".join([str(pizza["id"]) for pizza in team["pizzas"]])
+                        file.write(line)
+
+
+class Solver(IO):
     def solve(self):
+        self.fill_teams_with_pizzas()
+        self.write_output()
+
+    def fill_teams_with_pizzas(self):
         self.sorted_pizza_list = sorted(self.pizza_list, key=self.distance_from_mean, reverse=True)
+        self.assign_pizzas_by_best_points_possible()
+        self.assign_reusable_pizzas_to_leftover_teams()
 
-        while self.filled_teams_count < self.teams_count:
-            filled_before = self.filled_teams_count
+    def distance_from_mean(self, pizza):
+        return (len(pizza["ingredients"]) - len(self.all_ingredients) / 2.0) ** 2
 
-            print("Assigning pizzas everywhere by best points possible")
+    def assign_pizzas_by_best_points_possible(self):
+        all_teams_count = sum(self.team_count_dict.values())
+        while self.full_teams_count < all_teams_count:
+
+            filled_before = self.full_teams_count
+
+            print(self.assign_pizzas_by_best_points_possible.__name__)
             for _ in tqdm(range(len(self.sorted_pizza_list))):
-                best_pizza = self.sorted_pizza_list.pop()
-                best_team = self.add_pizza_to_best_team(best_pizza)
 
+                best_pizza = self.sorted_pizza_list.pop()
+                best_team = self.find_best_team_for_pizza(best_pizza, 10000)
                 if best_team is None:
                     break
+                else:
+                    self.add_pizza_to_team(best_pizza, best_team)
 
-            self.sorted_pizza_list += sorted(self.get_reusable_pizzas(), key=self.distance_from_mean, reverse=True)
-            if len(self.sorted_pizza_list) == 0:
+            self.sorted_pizza_list = sorted(
+                self.sorted_pizza_list + self.get_reusable_pizzas(),
+                key=self.distance_from_mean,
+                reverse=True
+            )
+
+            if self.full_teams_count == filled_before or len(self.sorted_pizza_list) == 0:
                 break
 
-            filled_after = self.filled_teams_count
+    def find_best_team_for_pizza(self, pizza, team_loop_limit):
+        best_score = None
+        best_team = None
 
-            if filled_after == filled_before:
-                break
+        team_loop_id = 0
+        for pizza_count in self.team_dict:
+            for team in self.team_dict[pizza_count]:
+                if len(team["pizzas"]) < team["max_pizzas"]:
+                    score = self.get_team_pizza_score(team, pizza)
 
+                    if best_score is None or score > best_score:
+                        best_score = score
+                        best_team = team
+
+                if team_loop_id > team_loop_limit:
+                    return best_team
+                team_loop_id += 1
+
+        return best_team
+
+    @staticmethod
+    def get_team_pizza_score(team, pizza):
+        current_ingredients = {ingredient for pizza in team["pizzas"] for ingredient in pizza["ingredients"]}
+
+        new_score = len(current_ingredients.union(pizza["ingredients"])) ** 2
+        common_ingredients_squared = (len(current_ingredients) + len(pizza["ingredients"])) / 2.0
+
+        return new_score - common_ingredients_squared
+
+    def get_reusable_pizzas(self):
+        reusable_pizzas = []
+
+        for pizza_count in self.team_dict:
+            for team in self.team_dict[pizza_count]:
+                if team["max_pizzas"] != len(team["pizzas"]):
+                    reusable_pizzas += team["pizzas"]
+                    self.team_dict[pizza_count][team["id"]]["pizzas"] = []
+
+        return reusable_pizzas
+
+    def assign_reusable_pizzas_to_leftover_teams(self):
         if len(self.sorted_pizza_list) == 0:
             return
 
         leftover_teams = self.get_leftover_teams()
 
-        print("Assigning reusable pizzas to leftover teams")
+        print(self.assign_reusable_pizzas_to_leftover_teams.__name__)
         for _ in tqdm(range(len(leftover_teams))):
-            filled_before = self.filled_teams_count
+            filled_before = self.full_teams_count
 
-            current_team = leftover_teams.pop()
+            team = leftover_teams.pop()
             while len(self.sorted_pizza_list):
-                result = self.assign_best_pizza_to_team(current_team)
-
-                if result is False:
+                self.assign_best_pizza_to_team(team, 10000)
+                if self.is_team_full(team):
                     break
 
-            filled_after = self.filled_teams_count
-
-            if filled_after == filled_before:
+            if self.full_teams_count == filled_before:
                 break
-
-        self.print_result()
-
-    def distance_from_mean(self, pizza):
-        return (pizza["ingredients_count"] - len(self.all_ingredients) / 2.0) ** 2
-
-    def add_pizza_to_best_team(self, best_pizza):
-        best_score = None
-        best_team_id = None
-        best_count = None
-
-        loop_id = 0
-        for count in self.team_dict:
-            team_id = 0
-            for team in self.team_dict[count]["list"]:
-                if len(team["pizzas"]) < team["max_pizzas"]:
-                    score = self.get_score(team, best_pizza)
-
-                    if best_score is None or score > best_score:
-                        best_score = score
-                        best_team_id = team_id
-                        best_count = count
-
-                team_id += 1
-                loop_id += 1
-
-                if loop_id > 10000:
-                    break
-
-            if loop_id > 10000:
-                break
-
-        if best_team_id is not None:
-            self.team_dict[best_count]["list"][best_team_id]["pizzas"].append(best_pizza)
-
-            current_team = self.team_dict[best_count]["list"][best_team_id]
-            if len(current_team["pizzas"]) == current_team["max_pizzas"]:
-                self.filled_teams_count += 1
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def get_score(team, pizza):
-        current_ingredients = {
-            i for p in team["pizzas"] for i in p["ingredients"]
-        }
-
-        return len(current_ingredients.union(pizza["ingredients"])) ** 2 - (
-                len(current_ingredients) + pizza["ingredients_count"]) / 2.0
-
-    def print_result(self):
-        with open(self.output_path_prefix + self.file_name, 'w+') as file:
-            file.write(str(self.filled_teams_count) + "\n")
-
-            for count in self.team_dict:
-                for team in self.team_dict[count]["list"]:
-                    if team["max_pizzas"] == len(team["pizzas"]):
-                        line = str(team["max_pizzas"]) + " " + " ".join([str(p["id"]) for p in team["pizzas"]])
-                        file.write(line + "\n")
-
-    def get_reusable_pizzas(self):
-        reusable_pizzas = []
-
-        for count in self.team_dict:
-            for team in self.team_dict[count]["list"]:
-                if team["max_pizzas"] != len(team["pizzas"]):
-                    reusable_pizzas += team["pizzas"]
-                    self.team_dict[count]["list"][team["id"]]["pizzas"] = []
-
-        return reusable_pizzas
 
     def get_leftover_teams(self):
         leftover_teams = []
 
-        for count in ["4", "3", "2"]:
-            for team in self.team_dict[count]["list"]:
+        for pizza_count in reversed(self.team_count_dict.keys()):
+            for team in self.team_dict[pizza_count]:
                 if team["max_pizzas"] != len(team["pizzas"]):
                     leftover_teams.append(team)
 
         return leftover_teams
 
-    def assign_best_pizza_to_team(self, current_team):
-        if not len(self.sorted_pizza_list):
-            return False
+    def assign_best_pizza_to_team(self, team, pizza_loop_limit):
+        best_score = None
+        best_pizza_loop_id = None
 
-        max_score = None
-        best_loop_id = None
-        loop_id = 0
+        for pizza_loop_id, pizza in enumerate(self.sorted_pizza_list):
+            score = self.get_team_pizza_score(team, pizza)
 
-        for possible_pizza in self.sorted_pizza_list:
-            score = self.get_score(current_team, possible_pizza)
+            if best_score is None or score > best_score:
+                best_score = score
+                best_pizza_loop_id = pizza_loop_id
 
-            if max_score is None or score > max_score:
-                max_score = score
-                best_loop_id = loop_id
+            if pizza_loop_id > pizza_loop_limit:
+                break
 
-            loop_id += 1
-
-        best_pizza = self.sorted_pizza_list.pop(best_loop_id)
-        return self.add_pizza_to_team(best_pizza, current_team)
+        best_pizza = self.sorted_pizza_list.pop(best_pizza_loop_id)
+        self.add_pizza_to_team(best_pizza, team)
 
     def add_pizza_to_team(self, pizza, team):
-        self.team_dict[str(team["max_pizzas"])]["list"][team["id"]]["pizzas"].append(pizza)
-        updated_team = self.team_dict[str(team["max_pizzas"])]["list"][team["id"]]
+        self.team_dict[str(team["max_pizzas"])][team["id"]]["pizzas"].append(pizza)
 
-        if len(updated_team["pizzas"]) == updated_team["max_pizzas"]:
-            self.filled_teams_count += 1
-            return False
+        if self.is_team_full(team):
+            self.full_teams_count += 1
 
-        return True
+    @staticmethod
+    def is_team_full(team):
+        return len(team["pizzas"]) == team["max_pizzas"]
 
 
 if __name__ == '__main__':
